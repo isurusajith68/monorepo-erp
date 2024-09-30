7add114bd38bce139aaec83313797fc1e8dc4add
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-09-30 15:54:23 +0530

    test lint

--- apps/test1/src/app/layout.tsx
+++ apps/test1/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>ERP</title>
+        <title>ERP </title>
       </head>
       <body className={inter.className}>
         <Toaster />
