6c2b132fd5e123648c7bb366189184b2c8481f6e
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-10-01 19:41:59 +0530

    add backend repo

---
+++ apps/erp-entry-backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const pool = require('./database')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.post('/booking', (req, res) => {
+  console.log(req.body)
+  res.send('response recived :' + req.body)
+})
+
+app.listen(4000, () => console.log('server is running on port 4000'))
