95f241f57ff7f78652bd58ff6bf59de7c7a9100d
Pasindu-Adhikari <pasindumax39@gmail.com>
2024-10-07 14:01:10 +0530

    test

--- apps/booking-app/src/components/Navbar/sidebar.tsx
+++ apps/booking-app/src/components/Navbar/sidebar.tsx
@@ -40,7 +40,7 @@ const Sidebar = () => {
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
             >
               <Home className="h-4 w-4" />
-              Dashboard
+              Dashboard test
             </Link>
             <Link
               to="/booking/add"
