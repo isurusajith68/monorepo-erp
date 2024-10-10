99cc5943c9e00c1ee6e4597463480d249f71166c
Pasindu-Adhikari <pasindumax39@gmail.com>
2024-10-10 15:05:30 +0530

    publish

--- apps/hotel-web/src/app/select-template/_component/template1/template1-home/footer-page.tsx
+++ apps/hotel-web/src/app/select-template/_component/template1/template1-home/footer-page.tsx
@@ -130,8 +130,7 @@ function FooterPage({
         {/* Footer Bottom */}
         <div className="max-w-7xl mx-auto px-4 mt-8 text-center text-gray-500">
           <p>
-            © {formData.name}, All Right Reserved. Designed By Ceyinfo
-            Soluation
+            © {formData.name}, All Right Reserved. Designed By Ceyinfo Soluation
           </p>
           <div className="flex justify-center space-x-4 mt-4">
             <a href="#" className="hover:text-yellow-500">
