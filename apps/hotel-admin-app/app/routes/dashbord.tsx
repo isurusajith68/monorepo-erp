2d27ebf19eaedb9d660246679ce2e5a8d65f0ae6
Kasun Deshapriya <132161149+kasun-deshapriya@users.noreply.github.com>
2024-10-10 09:02:38 +0530

    "Added dependencies and configurations for hotel-admin-app in pnpm-lock.yaml"

---
+++ apps/hotel-admin-app/app/routes/dashbord.tsx
@@ -0,0 +1,13 @@
+
+
+function dashbord() {
+  return (   
+      <div className='ml-[18.4%] h-screen'>
+        <div>
+        <h1>Dashboard</h1>
+        </div>
+      </div>
+  )
+}
+
+export default dashbord
