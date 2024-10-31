6ec748d3cccc9a3706aada5735638ca8ab9c3189
97ish07 <ishani9799@gmail.com>
2024-10-31 11:50:15 +0530

    remove admin app

--- apps/hotel-admin-app/app/hooks/use-toast.ts
+++
@@ -1,9 +0,0 @@
-import { toast as toastify } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-
-export function useToast() {
-  return {
-    success: (message: string) => toastify.success(message),
-    error: (message: string) => toastify.error(message),
-  }
-}
